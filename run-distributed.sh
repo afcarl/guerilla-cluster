4b1281f19db94582dde640b587586b358ce34f6b
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-04-08 01:41:58 -0400

    Add more nodes subgroups

--- run-distributed.sh
+++ run-distributed.sh
@@ -2,7 +2,7 @@
 SCRIPT=$1
 REMOTE_USER=username
 
-function enumerate_machines
+function enumerate_lab_machines
 {
     for i in $(seq $2)
     do
@@ -10,6 +10,20 @@ function enumerate_machines
     done
 }
 
-enumerate_machines lab1 16 | xargs -P16 -n1 -I{}\
+function enumerate_machines
+{
+    enumerate_lab_machines lab1 16 # Quad i5
+    enumerate_lab_machines lab6 28 # Quad i5
+    enumerate_lab_machines lab2 49 # Core 2
+    enumerate_lab_machines lab7 28 # Core 2
+
+#    enumerate_lab_machines lab3 16 # Down
+#    enumerate_lab_machines lab4 18 # Down
+#    enumerate_lab_machines lab5 18 # Down
+#    enumerate_lab_machines lab9 10 # Down
+
+}
+
+enumerate_machines | xargs -P50 -n1 -I{}\
     bash -c "ssh -q -x -o StrictHostKeyChecking=no -o ConnectTimeout=1 -o BatchMode=yes\
     {} 'cat | bash /dev/stdin' < $SCRIPT || true"
