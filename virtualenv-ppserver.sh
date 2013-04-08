3680ab2b7108c62f7d14eae60a11487f136dd21c
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2013-04-08 01:41:03 -0400

    Add hostname suffix to ppserver logs

--- virtualenv-ppserver.sh
+++ virtualenv-ppserver.sh
@@ -1,7 +1,7 @@
 export LD_CONFIG_PATH=$LD_CONFIG_PATH:$HOME/workspace/virtualenv/lib
 source $HOME/workspace/virtualenv/bin/activate
 cd $HOME/workspace/skill-acquisition/experiment
-nohup ppserver.py -a -t 60 > ppserver.out 2> ppserver.err < /dev/null &
+nohup ppserver.py -a -t 60 > ppserver-$HOSTNAME.out 2> ppserver-$HOSTNAME.err < /dev/null &
 
 NCPUS=$(cat /proc/cpuinfo | grep processor | wc -l)
 echo $HOSTNAME $NCPUS
